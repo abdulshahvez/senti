746b71b70e956e6493876723bad8925b00b36961
A.Q.Shahvez <113885062+abdulshahvez@users.noreply.github.com>
2024-08-06 14:07:26 +0530

    Update server.js

--- backend-senti/server.js
+++ backend-senti/server.js
@@ -4,7 +4,7 @@ const vader = require('vader-sentiment');
 const cors = require('cors');
 
 const app = express();
-const port = 5000;
+const port =  process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
